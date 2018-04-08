6d8064c68a2c374bfbc3d8c3450c63616b89bc1e
Tanya Pazitny <tpazitny@gmail.com>
2018-04-07 20:29:24 -0700

    Add hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  def hello
+    render html: "Hello, world!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#hello'
+  
   post '/servers/reservations', to: 'reservations#show'
   post '/servers/reserve', to: 'reservations#reserve'
   post '/servers/unreserve', to: 'reservations#unreserve'
